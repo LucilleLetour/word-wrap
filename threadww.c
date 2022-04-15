5c099bd70c7e24461707c1fbcb987b4416201f24
joshyeram <joshyc423@gmail.com>
2022-04-14 21:28:16 -0400

    no more cwd

--- threadww.c
+++ threadww.c
@@ -16,6 +16,101 @@
 
 typedef enum bool { false = 0, true = 1 } bool;
 
+int wrap(int fdr, int fdw, int line_length)
+{
+	int pstage = 0;
+	char c[1];
+	char word[5192];
+	int wordlen = 0;
+	int linelen = 0;
+	int pos = 0;
+	bool failed = false;
+	bool ins_space = false;
+	char lst, llst;
+	while (read(fdr, c, 1)) {
+		if (c[0] == ' ' || c[0] == '\n') { // Handle ws
+			if (wordlen > 0) { // Add the word that's been being built first
+				if (wordlen > line_length) { // The word is longer than the wrap length; report failure
+					failed = true;
+				}
+				if (pos > 0 && linelen + wordlen + (ins_space ? 1 : 0) > line_length && lst != '\n') { // Word (when added) will exceed wrap length, so add newline (unless it is the first word)
+					write(fdw, "\n", 1);
+					pos++;
+					linelen = 0;
+					ins_space = false;
+				}
+				if(ins_space) {
+					write(fdw, " ", 1);
+					pos++;
+					linelen++;
+				}
+				for (int i = 0; i < wordlen; i++) { // Add in the word
+					write(fdw, word + i, 1);
+					pos++;
+					linelen++;
+				}
+				wordlen = 0;
+			}
+			if (pos == 0) continue;
+
+			if (c[0] == '\n') {
+				if(pstage == 1) { // Previous character was also newline, must make new paragraph
+					write(fdw, "\n", 1);
+					pos++;
+					if(llst != '\n') {
+						write(fdw, "\n", 1);
+						pos++;
+					}
+					ins_space = false;
+					linelen = 0;
+					pstage = 2; // Directly proceding newlines are ignored
+				} else if(pstage == 0) {
+					pstage = 1;
+					ins_space = true;
+				}
+			} else if (c[0] == ' ') {
+				if (pos != 0 && lst != ' ' && lst != '\n' && linelen <= line_length) { // Add space char if appropriate: no preceding whitespace and not overrunning line length
+					ins_space = true;
+				}
+			}
+		} else {
+			if(pstage == 1) {
+				if(c[0] != ' ' && lst != ' ' && lst != '\n') { // Add a space instead of the newline, if it's needed
+					write(fdw, " ", 1);
+					pos++;
+					linelen++;
+				}
+			}
+			pstage = 0;
+			word[wordlen] = c[0];
+			wordlen++;
+		}
+		llst = lst;
+		lst = c[0];
+	}
+	if (wordlen > 0) { // Repeat of above, add word into file in case EOF with word in buffer
+		if (wordlen > line_length) {
+			failed = true;
+		}
+		if (linelen + wordlen > line_length) {
+			write(fdw, "\n", 1);
+			linelen = 0;
+			pos++;
+		}
+		for (int i = 0; i < wordlen; i++) {
+			write(fdw, word + i, 1);
+			pos++;
+			linelen++;
+		}
+		wordlen = 0;
+	}
+	if(pos > 0) {
+		write(fdw, "\n", 1);
+		pos++;
+	}
+	return failed ? EXIT_FAILURE : EXIT_SUCCESS;
+}
+
 typedef struct node 
 {
     char* dirName;
@@ -118,18 +213,24 @@ int directoryWorker(queue* dir, queue* file)
     DIR *givenDir = opendir(deqNode->dirName);
     if(givenDir == NULL)
     {
-        printf("invalid directory in queue\n");
+        printf("invalid directory in queue of |%s| \n", deqNode->dirName);
     }
     struct dirent *currDir;
     currDir = readdir(givenDir);
     struct stat checkDir;
 
-    chdir(deqNode->dirName);
-
     while(currDir!=NULL)
     {
-        if(DEBUG)printf("checking dir: %s \n", currDir->d_name);
-        stat(currDir->d_name, &checkDir);
+        int dirStrLen = strlen(deqNode->dirName);
+        int tempDirStrLen = strlen(currDir->d_name);
+        char* newDir = (char*)malloc(dirStrLen + tempDirStrLen + 2);
+        memcpy(newDir, deqNode->dirName, dirStrLen);
+        newDir[dirStrLen] = '/';
+        memcpy(&newDir[dirStrLen+1], currDir->d_name, tempDirStrLen);
+        newDir[dirStrLen+1+tempDirStrLen] = '\0';
+
+        if(DEBUG)printf("checking dir: %s \n", newDir);
+        stat(newDir, &checkDir);
         if(currDir->d_name[0]=='.')//. case
         {
             if(DEBUG)printf("ignored bc of .: %s \n",currDir->d_name);
@@ -147,14 +248,6 @@ int directoryWorker(queue* dir, queue* file)
         }
         if(S_ISDIR(checkDir.st_mode))//check if a directory is a folder
         {
-            
-            int dirStrLen = strlen(deqNode->dirName);
-            int tempDirStrLen = strlen(currDir->d_name);
-            char* newDir = (char*)malloc(dirStrLen + tempDirStrLen + 2);
-            memcpy(newDir, deqNode->dirName, dirStrLen);
-            newDir[dirStrLen] = '/';
-            memcpy(&newDir[dirStrLen+1], currDir->d_name, tempDirStrLen);
-            newDir[dirStrLen+1+tempDirStrLen] = '\0';
             enqueue(newDir, NULL, dir);
             if(DEBUG)printf("adding |%s| to dirQueue\n",newDir);
             currDir = readdir(givenDir);
@@ -178,6 +271,10 @@ int directoryWorker(queue* dir, queue* file)
 
 int wwWorker(queue* file, int line_length)
 {
+    node* deqNode = (node*)malloc(sizeof(node));
+    dequeue(deqNode, file);
+    
+    
     return 0;
 }
 
@@ -241,6 +338,10 @@ int main(int argc, char** argv)
     printQueue(directory);
     directoryWorker(directory, file);
     printQueue(directory);
+    directoryWorker(directory, file);
+    printQueue(directory);
     printQueue(file);
+
+    char buff[FILENAME_MAX];
     return EXIT_SUCCESS;
 }
